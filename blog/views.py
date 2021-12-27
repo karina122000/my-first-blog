2868a9f61ced68517c6fd9f41d55e68d8a4ea3d6
karina <karina122000@mail.ru>
2021-12-28 03:55:57 +0600

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,4 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html')
