1f57a9866ea7672f2e6a2eaae4e11713082e2658
karina <karina122000@mail.ru>
2021-12-28 20:16:32 +0600

    124

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, HttpResponseNotFound
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+
 
 
 
@@ -27,3 +28,29 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_delete(request, pk):
+    try:
+        post = get_object_or_404(Post, pk=pk)
+        post.delete()
+        return HttpResponseRedirect("/")
+
+
+    except post.DoesNotExist:
+        return HttpResponseNotFound("<h2>Post not found</h2>")
